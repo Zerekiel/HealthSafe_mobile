0bb501a050fbaa7c38024a579071a3a2f8cb27e6
KosmosUniverse <ducret.thomas@gmail.com>
2019-10-19 14:33:16 +0100

    first push after clean master

---
+++ app/src/main/java/com/eipteam/healthsafe/ChooseDisplay.java
@@ -0,0 +1,27 @@
+package com.eipteam.healthsafe;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_display);
+    }
+
+    public void choosePortable(View view)
+    {
+        Intent intent = new Intent(this, NFCConnect.class);
+        startActivity(intent);
+    }
+
+    public void deconnection(View view)
+    {
+        finish();
+    }
+}
+
